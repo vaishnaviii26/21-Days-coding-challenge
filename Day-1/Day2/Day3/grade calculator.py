10d16d284d1be972857f17b0f3867eded1b3fd75
vaishnaviii26 <vaishnaviveeramalai6-8@gmail.com>
2026-02-19 23:22:40 +0530

    Day3

---
+++ Day-1/Day2/Day3/grade calculator.py
@@ -0,0 +1,9 @@
+m=int(input())
+if(m>=90):
+    print("A grade")
+elif(75<=m<=89):
+    print(" B grade")
+elif(50<=m<=74):
+    print("C grade")
+else:
+    print("Fail")            
---
+++ Day-1/Day2/Day3/largest of 3 no.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if(a>b):
+    print("A larger")
+elif(b>c):
+    print("B larger")
+else:
+    print("C larger")    
