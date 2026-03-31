e2eeb09680ac632909fd26b8c607bb04f0f496b2
vaishnaviii26 <vaishnaviveeramalai6-8@gmail.com>
2026-03-31 20:19:04 +0530

    test

---
+++ BASIC OF PROGRAMMING TEST/Que5.py
@@ -0,0 +1,11 @@
+ 
+
+list = ["INFO", "ERROR", "ERROR", "INFO", "ERROR", "ERROR", "ERROR", 
+"INFO"];
+total_error = 0;
+
+for ERROR in list:
+    total_error += 1;
+
+
+print(f"Total Errors : {total_error}")
